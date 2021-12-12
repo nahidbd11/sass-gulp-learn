d63a6f09730abf45601999dfbd5af7d9123ae467
Nahid <diegonahid@gmail.com>
2021-12-12 08:51:41 +0600

    making css library

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 
 function buildStyles() {
-  return src("sass/index.scss").pipe(sass()).pipe(dest("css"));
+  return src("nsass/**/*.scss").pipe(sass()).pipe(dest("css"));
 }
 
 function watchTask() {
-  watch(["sass/index.scss"], buildStyles);
+  watch(["nsass/**/*.scss"], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
